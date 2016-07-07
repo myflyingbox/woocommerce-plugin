08e464c957c9b694a752bb48b6412e1668a32374
Thomas Belliard <thomas.belliard@sylogix.net>
2016-07-07 13:26:50 +0300

    Proper loading of MFB plugin

--- my-flying-box.php
+++ my-flying-box.php
@@ -20,40 +20,39 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 
+
 /**
  * Check if WooCommerce is active
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) :
 
-  require_once (dirname(__FILE__).'/../woocommerce/woocommerce.php');
+  /**
+   * Returns the main instance of My_Flying_Box to prevent the need to use globals.
+   *
+   * @since  1.0.0
+   * @return object My_Flying_Box
+   */
+  function MFB() {
+
+    // Load plugin class files
+    require_once( 'includes/class-my-flying-box.php' );
+    require_once( 'includes/class-my-flying-box-settings.php' );
+
+    // Load plugin libraries
+    require_once( 'includes/class-my-flying-box-admin-api.php' );
+    require_once( 'includes/class-my-flying-box-post-type.php' );
+    require_once( 'includes/class-my-flying-box-taxonomy.php' );
+    require_once( 'includes/class-my-flying-box-multiple-shipment.php' );
+
+    // Load view elements
+    require_once( 'includes/meta-boxes/class-mfb-meta-box-order-shipping.php' );
 
 
-  // Load plugin class files
-  require_once( 'includes/class-my-flying-box.php' );
-  require_once( 'includes/class-my-flying-box-settings.php' );
+    $instance = My_Flying_Box::instance( __FILE__, '1.0.0' );
 
-  // Load plugin libraries
-  require_once( 'includes/class-my-flying-box-admin-api.php' );
-  require_once( 'includes/class-my-flying-box-post-type.php' );
-  require_once( 'includes/class-my-flying-box-taxonomy.php' );
-  require_once( 'includes/class-my-flying-box-multiple-shipment.php' );
+    return $instance;
+  }
 
-  // Load view elements
-  require_once( 'includes/meta-boxes/class-mfb-meta-box-order-shipping.php' );
-
-/**
- * Returns the main instance of My_Flying_Box to prevent the need to use globals.
- *
- * @since  1.0.0
- * @return object My_Flying_Box
- */
-function MFB() {
-  
-	$instance = My_Flying_Box::instance( __FILE__, '1.0.0' );
-
-	return $instance;
-}
-
-MFB();
+  add_action( 'woocommerce_shipping_init', 'MFB' );
 
 endif;
