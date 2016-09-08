820eaddc98b8b64d45db3e9d4bc16c7c4723ec06
Thomas Belliard <thomas.belliard@sylogix.net>
2016-09-08 15:11:41 +0300

    Updating translations

--- includes/views/html-admin-carriers-weight-table.php
+++ includes/views/html-admin-carriers-weight-table.php
@@ -9,8 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		?>
 		<h3><?php printf( __( 'Weight/dimensions', 'my-flying-box' ) ); ?></h3>
-		<p><?php printf( __( 'When requesting a quote for a shipment, most carriers need parcel dimensions. The MyFlyingBox woocommerce shipping module uses this table to compute dimensions based on the total weight of the cart. There is no way for the module to automatically guess your final packaging based on a customer cart, so it is very important that you customize this table based on your product, average cart, and usual packaging strategy.', 'my-flying-box' ) ); ?></p>
-		
+		<p><?php printf( __('When requesting a quote for a shipment, most carriers need parcel dimensions. There are two ways to determine the dimensions used for automatic calculation: if dimensions of the products have been defined in the product catalog, these dimensions will be used (one parcel per item in the order, with the dimensions of the item) ; if no dimensions have been defined for the items in the order, the module will determine dimensions based on the total weight of the cart and the table below, associating weights to dimensions.','my-flying-box' )); ?></p>
+
 		<table class="<?php echo $current_section; ?> wc_input_table widefat">
 			<thead>
 				<tr>
@@ -22,11 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<th><?php _e( 'length', 'my-flying-box' ); ?></th>
 
 					<th><?php _e( 'width', 'my-flying-box' ); ?></th>
-					
+
 					<th><?php _e( 'height', 'my-flying-box' ); ?></th>
 
 				</tr>
-				
+
 			</thead>
 			<tbody>
 				<?php	foreach ( $dimensions as $key => $dimension ) {	?>
@@ -44,7 +44,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						<td class="dimension">
 							<input type="text" name="width_<?php echo $dimension->index; ?>" id="width_<?php echo $dimension->index; ?>" value="<?php echo $dimension->width; ?>" class="" /> <span>cm</span>
 						</td>
-						
+
 						<td class="dimension">
 							<input type="text" name="height_<?php echo $dimension->index; ?>" id="height_<?php echo $dimension->index; ?>" value="<?php echo $dimension->height; ?>" class="" /> <span>cm</span>
 							<input type="hidden" name="id_<?php echo $dimension->index; ?>" id="id_<?php echo $dimension->index; ?>" value="<?php echo $dimension->id; ?>" />
