afc6e8bf2b45609919dc8237617d6dfbff79a15b
Thomas Belliard <thomas.belliard@sylogix.net>
2023-08-09 18:04:18 +0300

    Release v0.16

--- my-flying-box.php
+++ my-flying-box.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: My Flying Box
- * Version: 0.15
+ * Version: 0.16
  * Plugin URI: http://www.myflyingbox.com
  * Description: Integrated Shipping services through My Flying Box API.
  * Author: Thomas Belliard (My Flying Box)
@@ -59,7 +59,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
     require_once( 'includes/meta-boxes/class-mfb-meta-box-order-shipping.php' );
     require_once( 'includes/meta-boxes/class-mfb-meta-box-bulk-order.php' );
 
-    $instance = My_Flying_Box::instance( __FILE__, '0.15' );
+    $instance = My_Flying_Box::instance( __FILE__, '0.16' );
 
     return $instance;
   }
